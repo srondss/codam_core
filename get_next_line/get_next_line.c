59e7bb32c2bd6fadafa97d25d99c9a3605d9313d
Youssef Srondy <srondyyoussef@gmail.com>
2022-11-01 12:41:06 +0100

    push

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ysrondy <ysrondy@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 12:40:45 by ysrondy           #+#    #+#             */
-/*   Updated: 2022/10/27 19:01:09 by ysrondy       ########   odam.nl         */
+/*   Updated: 2022/10/30 17:06:31 by ysrondy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -15,40 +15,66 @@
 #include <stdio.h>
 #include "../libft/libft.h"
 
+
+int	check_newline(char *buf)
+{
+	int i;
+
+	i = 0;
+	while (buf[i] != '\0')
+	{
+		if (buf[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*get_next_line(int fd)
 {
-	static char buf[BUFFER_SIZE + 1];
+	static char buf[BUFFER_SIZE];
 	static char *retstr;
-	static char *tmp;
 	int i;
 	int j;
+	static int k;
 
 	i = 0;
 	j = 1;
-
+	k = 0;
 	while (j != 0)
 	{
 		j = read(fd, buf, BUFFER_SIZE);
-		buf[BUFFER_SIZE] = '\0';
-		retstr = malloc(sizeof(char) * BUFFER_SIZE + 1);
-		while (buf[i] != '\0')
+		if (check_newline(buf) == 0)
 		{
-			retstr[i] = buf[i];
-			if (buf[i] == '\n')
-				break;
-			i++;
+			printf("Buf: %s\n", buf);
+			printf("Ret: %s\n", retstr);
+			if (!retstr)
+				retstr = ft_strdup(buf);
+			else
+			{
+		
+				//printf("buflen %d\n", ft_strlen(buf));
+				printf("strlen1 %d\n", ft_strlen(retstr));
+				retstr = malloc(sizeof(char) * (BUFFER_SIZE + ft_strlen(retstr) + 1));
+				//printf("strlen2 %d\n", ft_strlen(retstr));
+				while (buf[i] != '\0')
+				{
+					retstr[k] = buf[i];
+					if (buf[i] == '\n')
+					{
+						retstr[k + 1] = '\0';
+						break;
+					}
+					i++;
+					k++;
+				}
+				i = 0;
+			}
 		}
-		retstr[i + 1] = '\0';
-		i = 0;
-		printf("Buf: %s\n", buf);
-		printf("Ret: %s\n", retstr);
-		tmp = ft_strjoin(retstr, buf);
-		printf("Tmp: %s\n", tmp);
-		free(retstr);
 	}
-
-	printf("Buf: %s\n", buf);
-	printf("Ret: %s\n", tmp);
+	printf("outside\n");
+	printf("Final Buf: %s\n", buf);
+	printf("Final Ret: %s\n", retstr);
 	return (retstr);
 }
 
