23bef5516466c7070a9331d5e442bf9fa7cf11a0
Youssef Srondy <srondyyoussef@gmail.com>
2022-11-27 19:48:33 +0100

    fixed a few bugs, still having issues with larger buffer sizes for some reason

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ysrondy <ysrondy@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 12:40:45 by ysrondy           #+#    #+#             */
-/*   Updated: 2022/11/04 13:07:39 by ysrondy       ########   odam.nl         */
+/*   Updated: 2022/11/27 19:47:24 by ysrondy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -14,22 +14,32 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-//#include "../libft/libft.h"
+#include "../libft/libft.h"
 
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	if (!dest && !src)
+	if (!dst && !src)
 		return (0);
-	while (i < n)
+	if (src > dst)
 	{
-		((char *)dest)[i] = ((char *)src)[i];
-		i++;
+		while (i < len)
+		{
+			((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+			i++;
+		}
 	}
-	return (dest);
+	else
+	{
+		while (len)
+		{
+			((unsigned char *) dst)[len - 1] = ((unsigned char *) src)[len - 1];
+			len--;
+		}
+	}
+	return (dst);
 }
 
 int	ft_strlen(char const *str)
@@ -44,34 +54,33 @@ int	ft_strlen(char const *str)
 
 char *buff_to_line(char *line, char *buf) //stops if buf[i] == \n
 {
-	char *tmp;
+	char *str;	
 	int i;
 	int len;
 
 	i = 0;
-	if (!line)
+	len = ft_strlen(line);
+//	printf("Line in buff_to_line: %s\n", line);
+//	printf("Length of Line: %d\n", len);
+	str = (char *)malloc(sizeof(char) * len + BUFFER_SIZE + 1);
+	ft_strlcpy(str, line, (len + 1));
+//	printf("Str in buff_to_line: %s\n", str);
+	while (buf[i] != '\0' && buf[i] != '\n')
 	{
-		line = malloc(sizeof(char) * BUFFER_SIZE + 1);
-		ft_memcpy(line, buf, (BUFFER_SIZE + 1));
+		str[len + i] = buf[i];
+		i++;
+	}
+	if (buf[i] == '\n')
+	{
+		str[len + i] = '\n';
+		str[len + i + 1] = '\0';
 	}
 	else
-	{
-		len = ft_strlen(line);
-		tmp = malloc(sizeof(char) * len + 1);
-		ft_memcpy(tmp, line, (len + 1));
-		free(line);
-		line = malloc(len + BUFFER_SIZE + 1);
-		ft_memcpy(line, tmp, (ft_strlen(tmp)));
-		free(tmp);
-		while (buf[i] != '\0')
-		{
-			line[len] = buf[i];
-			len++;
-			i++;
-		}
-		line[len] = '\0';
-	}
-	i = 0;
+		str[len + i] = '\0';
+//	printf("Final_Str in buff_to_line: %s\n", str);
+	free(line);
+	line = ft_strdup(str);
+	free(str);
 	return (line);
 }
 
@@ -107,56 +116,66 @@ int	check_newline(char *buf)
 	while (buf[i] != '\0')
 	{
 		if (buf[i] == '\n')
-			return (1);
+			return (i + 1);
 		i++;
 	}
 	return (0);
 }
 
-char *line_update(char *line)
+char	*buf_update(char *buf)
 {
 	int i;
-	int j;
 
 	i = 0;
-	j = 0;
-	while (line[i] != '\n' && line[i] != '\0')
+//	printf("Buf dans buf_update: %s\n", buf);
+	while (buf[i] != '\n' && buf[i] != '\0')
 		i++;
-	while (line[i] != '\0') // SEGFAULT here EOF 
+	if (buf[i] != '\0')
 	{
-		line[j] = line[i + 1];
-		j++;
-		i++;
+//		printf("length Buf: %d\n", ft_strlen(buf));
+		ft_memmove(buf, buf+(i + 1), (ft_strlen(buf) - (i - 1)));
 	}
-	return (line);
+	return (buf); 
 }
 
 char	*get_next_line(int fd)
 {
-	static char buf[BUFFER_SIZE + 1];
-	static char *line;
-	static char *retstr;
+	static char 	buf[BUFFER_SIZE + 1];
+	char 		*line;
 	static int 	read_ret;
+	int		i;
+	int		j;
 
-	if (line)
-		line = line_update(line);
-	if (retstr)
-		free(retstr);
+	i = 0;
+	j = 0;
 	read_ret = BUFFER_SIZE;
-	// if buf[0]
-	// 	copy whatever was inside buffer into line 
+	line = ft_calloc(sizeof(char *), BUFFER_SIZE + 1);
+//	printf("Begin Buffer: %s\n", buf);
+	if (buf[0] != '\0')
+	{
+		while (buf[i] != '\0')
+		{
+	//		printf("enter\n");
+			line[j] = buf[i];
+			i++;
+			j++;
+		}
+	}
+//	printf("Line before start: %s\n", line);
 	while (!check_newline(line) && read_ret == BUFFER_SIZE)
 	{
 		read_ret = read(fd, buf, BUFFER_SIZE);
 		if (read_ret == 0 && buf[0] == '\0')
 			return (free(line), NULL);
 		if (read_ret == -1)
-			return ("Error, Could'nt read file.");
+			return (free(line), NULL);
 		buf[read_ret] = '\0';
+	//	printf("Updated Buffer: %s\n", buf);
 		line = buff_to_line(line, buf);
+	//	printf("Line: %s\n", line);
 	}
-	retstr = copy_newline(line);
-	return (retstr);
+	buf_update(buf);
+	return (line);
 }
 
 int	main()
@@ -176,3 +195,18 @@ int	main()
 	system("leaks -q a.out");
 	close(fd);
 }
+
+/* imagine i have the following text: he\nllo\nty
+and my BUFFER_SIZE=4. it reads the first four letters into my BUF: h-e-\n-l + '\0'
+my buff_to_line should copy up to the \n. So my line is: h-e-\n; 
+then my buf_update function needs to put move the l to the front of the BUF.
+Then I return the line: he\n. 
+Then I call the function again but before I start reading into my buffer, I need to copy the l into my line. 
+So my line would start of with being l only.
+Then i read into my buffer the next four letters: llo\n;
+Then I make sure that i start copying from index of ft_strlen(line) instead of 0. and repeat. 
+Question: Why do I need to update my buffer? Because if I don't i will duplicate what was written inside due to it being static.
+I think I indeed need to update the buffer.*/   
+
+// get_next_line % gcc -Wall -Wextra -Werror get_next_line.c -D BUFFER_SIZE=4 ../libft/ft_calloc.c ../libft/ft_strlcpy.c ../libft/ft_bzero.c ../libft/ft_strdup.c
+
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,66 @@
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)s)[i] = '\0';
+		i++;
+	}
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	if (size == 0)
+		return (len);
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (len);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	if (size != 0 || nmemb != 0)
+		ft_bzero(ptr, nmemb * size);
+	return ((void *) ptr);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	ptr = (char *)malloc(sizeof(char) * (i + 1));
+	if (!(ptr))
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
