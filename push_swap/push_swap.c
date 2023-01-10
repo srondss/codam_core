ec1c7395d715c278a6d1387d4ee0bf359d01348f
Youssef Srondy <srondyyoussef@gmail.com>
2023-01-10 22:04:03 +0100

    Yay! Fixed my algorithm mistakes, took me a while but made it! Super fun stuff. I'm now thinking of optimising it by creating a function which checks if the list is sorted to avoid doing a lot of the heavy lifting if it's not necessary. Will work on this tomorrow :)

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ysrondy <ysrondy@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 10:38:23 by ysrondy           #+#    #+#             */
-/*   Updated: 2023/01/09 19:33:23 by ysrondy          ###   ########.fr       */
+/*   Updated: 2023/01/10 21:54:09 by ysrondy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,24 +136,29 @@ int main_1(int argc, char **argv)
 	{
 		struct s_stack *head_b;
 		int		i;
-
+		
+	// if stack b is empty, just push a to b.
 		if (!stack_b)
 			pb(&stack_a, &stack_b);
+	// if top stack a is bigger than top stack b, just push to stack b.
 		else if (stack_a->number > stack_b->number)
 			pb(&stack_a, &stack_b);
 		else
 		{
 			i = 0;
 			head_b = stack_b;
+	// while top of stack a is bigger than stack b, increment i and then go to next node in stack b.
 			while (stack_a->number < head_b->number)
 			{
-				if (stack_b->prev == stack_b->next)
+	//		if there's only 1 node in stack b, then just push a to b and swap both.
+				if (stack_b->prev == stack_b->next && stack_b->next->number == stack_b->number)
 				{
 					pb(&stack_a, &stack_b);
 					sb(&stack_b);
 					break;
 				}
 				head_b = head_b->next;
+	//		if you went through all the nodes, increment + break;
 				if (head_b == stack_b)
 				{
 					i++;
@@ -161,23 +166,31 @@ int main_1(int argc, char **argv)
 				}
 				i++;
 			}
+	//		if stack b has more than 1 node but only 1 number is bigger than top of stack a
+	//		the just push a to b and swap.
 			if (i == 1)
 			{
 				pb(&stack_a, &stack_b);
 				sb(&stack_b);
 			}
-			else
+			else if (i > 1)
 			{
+	//		while there are i numbers bigger than top of stack a, push b to a and rotate so 
+	//		top of stack a is still the same initial number.
 				while (i > 0)
 				{
 					pa(&stack_a, &stack_b);
 					ra(&stack_a);
 					i--;
 				}
+	//		when you've pushed everything back to a, push a to b so the initial number is now at top of stack b.
 				pb(&stack_a, &stack_b);
 			}
 		}
+//debugging	print_stack(&stack_a, 'a');
+//debugging	print_stack(&stack_b, 'b');
 	}
+	// push everything back to stack a since it's sorted.
 	while (stack_b)
 		pa(&stack_a, &stack_b);
 //	End algorithm.
