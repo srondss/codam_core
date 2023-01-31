fc9cdc0afaeeb2433d5660857749df6febec642a
Youssef Srondy <srondyyoussef@gmail.com>
2023-01-31 19:11:21 +0100

    Was really confused initially. Now it's getting a bit better, commented my code to figure out exactly what is happening. Also added the MLX.h file in the root to have easier access and to refer to when looking for a specific function. Decent progress for Day 1 lol, not completely lost anymore.

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ysrondy <ysrondy@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 07:39:27 by ysrondy           #+#    #+#             */
-/*   Updated: 2023/01/31 08:00:22 by ysrondy          ###   ########.fr       */
+/*   Updated: 2023/01/31 18:40:03 by ysrondy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,58 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <memory.h>
-#define WIDTH 256
-#define HEIGHT 256
+#define WIDTH 500
+#define HEIGHT 500
 
-mlx_image_t	*g_img;
+mlx_image_t *g_img;
 
-void	hook(void *param)
+void hook(void *param)
 {
-	mlx_t	*mlx;
+	mlx_t *mlx;
 
 	mlx = param;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		g_img->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		g_img->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		g_img->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		g_img->instances[0].x += 5;
+	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+	/*	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+			g_img->instances[0].y -= 5;
+		if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+			g_img->instances[0].y += 5;
+		if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+			g_img->instances[0].x -= 5;
+		if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+			g_img->instances[0].x += 5;*/
 }
 
-int32_t	main(void)
+// Exit the program as failure.
+static void ft_error(void)
 {
-	mlx_t	*mlx;
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
 
+int32_t main(void)
+{
+	mlx_t *mlx;
+	//	Here you're essentially initialising a window with a certain width and height.
 	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
 	if (!mlx)
-		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(mlx, 128, 128);
+		ft_error();
+	//	Here you're creating a new image, which needs a pointer to the window, and how big it will be.
+	g_img = mlx_new_image(mlx, 100, 100);
+	if (!g_img)
+		ft_error();
+
+	//	Here you're setting the pixels of the image you've created to the color 255 (white),
+	// and you specify how many pixels are going to have that color. Why times 4?
+	// Since each pixel is represented as an integer, it will be four bytes for four channels.
+	// If you only do width * height, then you end up with only 1 / 4 of the image pixelized.
 	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
-	mlx_image_to_window(mlx, g_img, 0, 0);
-	mlx_loop_hook(mlx, &hook, mlx);
+
+	// Draw the image at coordinate (0, 0).
+	mlx_image_to_window(mlx, g_img, 200, 200);
+
+	// Run the main loop and terminate on quit.
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
